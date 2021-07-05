ba5073e834227928a5f9575cc8e17d976e908d98
dannguyenmessi1705 <79917180+dannguyenmessi1705@users.noreply.github.com>
2021-07-05 21:46:06 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "m.me/didannguyen.messi1705";
+            window.location = "facebook.com/didannguyen.messi1705";
           },
         });
       }
